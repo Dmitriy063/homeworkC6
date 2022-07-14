dc7788e3b8f89701d3599459719b0b1ca1baefcf
Dmitriy <rusich063@gmail.com>
2022-07-14 11:57:07 +0300

    Домашнее задание 6 семинар

---
+++ homeworkC6_01/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+int m = Prompt("Введите число: ");
+int[] massiveNumbers = new int[m];
+void input(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введи {i + 1} число: ");
+        massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+int Comparison(int[] massiveNumbers)
+{
+    int count = 0;
+    for (int i = 0; i < massiveNumbers.Length; i++)
+    {
+        if (massiveNumbers[i] > 0)
+            count += 1;
+    }
+    return count;
+}
+InputNumbers(m);
+Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
---
+++ homeworkC6_02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+int Prompt(string message)
+{
+    Console.Write(message);
+    int a = int.Parse(Console.ReadLine());
+    return a;
+}
+
+double b1 = Prompt("Введите число b1: ");
+double k1 = Prompt("Введите число k1: ");
+double b2 = Prompt("Введите число b2: ");
+double k2 = Prompt("Введите число k2: ");
+double x = (b2 - b1) / (k1 - k2);
+System.Console.WriteLine($"Точка пересечения ({x},{k2 * x + b2})");
